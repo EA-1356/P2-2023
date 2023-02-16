22a035cae6442c4708a70411b97d47b130e15246
TD-FRI <td.fri@gmail.com>
2023-02-16 12:31:34 +0100

    p1

---
+++ src/Odstevanje.java
@@ -0,0 +1,11 @@
+public class Odstevanje {
+    static void odstevanje(int n) {
+      for(int i=n; i>=0; i--) {
+        System.out.println(i);
+      }
+    }
+
+	public static void main(String[] args) {
+	  odstevanje(10);
+	}
+}
